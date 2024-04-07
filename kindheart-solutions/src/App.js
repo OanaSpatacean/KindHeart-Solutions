8633b876c41f127d6253ae179d27588f7c49db9f
Gabriela <gabrielavieriu01@gmail.com>
2024-04-07 19:37:32 +0300

    Nefinalizat

--- kindheart-solutions/src/App.js
+++ kindheart-solutions/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react' 
 import Nav from './components/nav'
+import Rout from './components/rout'
 
 
 import {BrowserRouter} from 'react-router-dom'
+import Home from './components/home'
 
 
 
@@ -12,7 +14,9 @@ const App = () =>{
   return (
     <>
     <BrowserRouter>
+
     <Nav/>
+    <Rout/>
     </BrowserRouter>
   
     </>
