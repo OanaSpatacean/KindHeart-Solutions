ac453d1707b444f1764780ca783301f26a8fdc10
Gabriela <gabrielavieriu01@gmail.com>
2024-04-22 19:42:21 +0300

    Am adaugat Proiectele mele

---
+++ kindheart-solutions/src/components/proiectelemele.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import './proiecte.css'
+import { AiFillEye, AiOutlineClose} from 'react-icons/ai';
+import { BiSolidDonateHeart } from "react-icons/bi";
+
+const Proiectelemele = ({shop}) => {
+    const [showDetail, setShowDetail] = useState(false)
+    const [detail, setDetail] = useState([])
+
+    const detailpage = (product) => 
+    {
+        const detaildata = ([{product}])
+        const productdetail = detaildata[0]['product']
+        // console.log(productdetail)
+        setDetail(productdetail)
+        setShowDetail(true)
+    }
+
+    const closedetail = () => 
+    {
+        setShowDetail(false)
+    }
+
+  return (
+    <>
+    {
+        showDetail ? 
+        <>
+        <div className='product_detail'>
+            <button className='close_btn' onClick={closedetail}><AiOutlineClose /></button>
+            <div className='container'>
+                <div className='img_box'>
+                    <img src={detail.Image} alt=''></img>
+                </div>
+                <div className='info'>
+                    <h4># {detail.County}</h4>
+                    <h2>{detail.Name}</h2>
+                    <p>{detail.Description}</p>
+                    <button onClick={()=>window.location.href="/editeaza/"+detail.id}>Editeaza</button>
+                </div>
+            </div>
+        </div>
+        </>
+        : null
+    }
+    <div className='shop'>
+        <h2># PROIECTELE MELE</h2>
+        <p>ACASA . PROIECTE</p>
+        <div className='container'>
+            <div className='left_box'>
+                <div className='category'>
+
+                </div>
+                <div className='banner'>
+                    <div className='img_box'>
+                        <img src='image/hearts.webp' alt=''></img>
+                    </div>
+                </div>
+            </div>
+            <div className='right_box'>
+                <div className='banner'>
+                    <div className='img_box'>
+                        <img src='image/Donate.jpg' alt=''></img>
+                    </div>
+                </div>
+                <div className='product_box'>
+                    <h2>Alege cauza care te-a impresionat cel mai mult</h2>
+                    <div className='product_container'>
+                        {
+                            shop.map((curElm) => 
+                            {
+                                return(
+                                    <>
+                                    <div className='box'>
+                                        <div className='img_box'>
+                                            <img src={curElm.Image} alt=''></img>
+                                            <div className='icon'>
+                                               <li onClick={() => detailpage (curElm)}><AiFillEye /></li> 
+                                            </div>
+                                        </div>
+                                        <div className='detail'>
+                                            <h3>{curElm.Name}</h3>
+                                            <button onClick={()=>window.location.href="/editeaza/"+curElm.id}>Editeaza</button>
+                                        </div>
+                                    </div>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Proiectelemele
